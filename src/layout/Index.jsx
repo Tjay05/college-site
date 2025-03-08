cb26c8778a467e0dbe74f4aa8cef316b20994f02
Tjay05 <tjayolatayo@gmail.com>
2025-03-08 15:38:17 +0100

    Add footer

--- src/layout/Index.jsx
+++ src/layout/Index.jsx
@@ -1,11 +1,14 @@
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-const AppLAyout = () => {
+const AppLayout = () => {
   return (
     <div>
       <Navbar/>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis nulla inventore maiores ab exercitationem vitae. Eligendi architecto voluptatibus eos ipsum quidem voluptates ratione ad praesentium est! Mollitia praesentium quidem amet!</p>
+      <Footer/>
     </div>
   );
 }
  
-export default AppLAyout;
+export default AppLayout;
