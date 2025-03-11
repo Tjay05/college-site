274d0edd7e274682fbe30687ee5ff5f551a5aa24
Tjay05 <tjayolatayo@gmail.com>
2025-03-11 21:46:46 +0100

    Add account page

---
+++ src/pages/account/Index.jsx
@@ -0,0 +1,39 @@
+import Active_Stud_Icon from "../../assets/icons/active-student.svg";
+import Inactive_Lecturer_Icon from "../../assets/icons/inactive-teacher.svg";
+import Active_Lecturer_Icon from "../../assets/icons/active-teacher.svg";
+import Inactive_Student_Icon from "../../assets/icons/inactive-student.svg";
+import { IoMdClose } from "react-icons/io";
+import { useContext } from "react";
+import { ModalContext } from "../../App";
+import { Link } from "react-router-dom";
+import SignupPopup from "./Signup";
+import { useState } from "react";
+
+const AccountPopup = () => {
+  const { setIsModalOpen, isModalOpen } = useContext(ModalContext);
+  const [formType, setFormType] = useState('student');
+
+  return (
+    <div className="absolute top-0 inset-0 backdrop-blur-2xl z-99">
+      <div className="bg-white p-6 rounded-lg mx-auto w-[90%] md:w-[75%] flex flex-col mt-12 md:mt-18">
+        <IoMdClose onClick={() => setIsModalOpen(!isModalOpen)} size={30} className='self-end cursor-pointer'/>
+        <div className="m-2 font-[Inter]">
+          <h2 className="text-[#41A94E] text-center font-bold text-xl md:text-4xl">Sign up to get started</h2>
+          <div className="border p-2 rounded-lg flex justify-between my-6 font-bold text-base md:my-10">
+            <div onClick={() => setFormType('student')} className={`role_button ${formType === 'student' ? 'bg-[#41A94E]' : 'bg-white'}`}>
+              <img src={`${formType === 'student' ? Active_Stud_Icon : Inactive_Student_Icon}`} alt="" className="w-6 lg:w-8" />
+              <p className={`${formType === 'student' ? 'text-white' : 'text-[#41A94E]'}`}>Student</p>
+            </div>
+            <div onClick={() => setFormType('lecturer')} className={`role_button ${formType === 'lecturer' ? 'bg-[#41A94E]' : 'bg-white'}`}>
+              <img src={`${formType === 'lecturer' ? Active_Lecturer_Icon : Inactive_Lecturer_Icon}`} alt="" className="w-6 lg:w-8" />
+              <p className={`${formType === 'lecturer' ? 'text-white' : 'text-[#41A94E]'}`}>Lecturer</p>
+            </div>
+          </div>
+          { formType === 'student' && <SignupPopup/>}
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default AccountPopup;
