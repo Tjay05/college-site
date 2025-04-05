9a9211c70d3fd2b5c543af8d9a967613a7b2063f
Tosin Ekundayo <tosinekshally@gmail.com>
2025-04-05 16:32:04 +0100

    worked on the user

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.docx'],
   plugins: [
     react(),
     tailwindcss(),
